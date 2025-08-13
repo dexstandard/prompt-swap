4dfc61ebc5cf5002e25aed75b5472312a6898582
dexstandard <alxndrmann@gmail.com>
2025-08-13 09:10:33 +0700

    Add dashboard route and create index navigation

--- frontend/src/routes/CreateIndex.tsx
+++ frontend/src/routes/CreateIndex.tsx
@@ -1,6 +1,6 @@
 import IndexForm from '../components/forms/IndexForm';
 
-export default function Index() {
+export default function CreateIndex() {
   return (
     <div className="flex justify-center p-4">
       <IndexForm />
