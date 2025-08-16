53ea4f5b9a47fab1970df03311ab1da8d4d4e419
dexstandard <alxndrmann@gmail.com>
2025-08-16 20:32:10 +0700

    Rename Settings to Keys and add icons to sidebar

--- frontend/src/routes/Keys.tsx
+++ frontend/src/routes/Keys.tsx
@@ -2,7 +2,7 @@ import { useUser } from '../lib/useUser';
 import AiApiKeySection from '../components/forms/AiApiKeySection';
 import ExchangeApiKeySection from '../components/forms/ExchangeApiKeySection';
 
-export default function Settings() {
+export default function Keys() {
   const { user } = useUser();
   if (!user) return <p>Please log in.</p>;
   return (
